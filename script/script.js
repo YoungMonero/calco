e92d4f1c146efb5a116ced89045c431951ddf0f8
youngmonero <yungmonero29@gmail.com>
2025-11-11 17:09:32 +0100

    linter spacing fixed

--- script/script.js
+++ script/script.js
@@ -1,40 +1,38 @@
-const inputValue = document.getElementById('userinput');
+const inputValue = document.getElementById('userinput')
 
-document.querySelectorAll('.operation').forEach((item) => {
-  item.addEventListener('click', (e) => {
+document.querySelectorAll('.operation').forEach(item => {
+  item.addEventListener('click', e => {
     const lastValue = inputValue.value.substring(
       inputValue.value.length - 1,
       inputValue.value.length
-    );
+    )
 
     if (e.target.innerText === '=') {
       try {
-        if (/^[0-9+\-*/.%() ]+$/.test(inputValue.value)) {
-          inputValue.value = Function('"use strict"; return (' + inputValue.value + ')')();
-        } else {
-          inputValue.value = 'Error';
-        }
-      } catch (err) {
-        inputValue.value = 'Error';
+        inputValue.value = calculate(inputValue.value)
+      } catch {
+        inputValue.value = 'Error'
       }
     } else if (e.target.innerText === 'AC') {
-      inputValue.value = '0';
+      inputValue.value = '0'
     } else if (e.target.innerText === 'DEL') {
-      inputValue.value = inputValue.value.substring(0, inputValue.value.length - 1);
-      if (inputValue.value.length === 0) {
-        inputValue.value = '0';
-      }
+      inputValue.value = inputValue.value.substring(0, inputValue.value.length - 1)
+      if (inputValue.value.length === 0) inputValue.value = '0'
     } else if (!isNaN(lastValue)) {
-      inputValue.value += e.target.innerText;
+      inputValue.value += e.target.innerText
     }
-  });
-});
+  })
+})
 
-document.querySelectorAll('.number').forEach((item) => {
-  item.addEventListener('click', (e) => {
-    if (inputValue.value === '0') {
-      inputValue.value = '';
-    }
-    inputValue.value += e.target.innerText.trim();
-  });
-});
+document.querySelectorAll('.number').forEach(item => {
+  item.addEventListener('click', e => {
+    if (inputValue.value === '0') inputValue.value = ''
+    inputValue.value += e.target.innerText.trim()
+  })
+})
+
+function calculate(expr) {
+  if (!/^[0-9+\-*/.%() ]+$/.test(expr)) throw new Error('Invalid input')
+  expr = expr.replace(/%/g, '/100')
+  return new Function('return ' + expr)()
+}
