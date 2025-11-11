f26999e348a0416bd40e75845417de4dd7b79490
youngmonero <yungmonero29@gmail.com>
2025-11-11 16:07:29 +0100

    added the requied changes

--- script/script.js
+++ script/script.js
@@ -1,37 +1,35 @@
 const inputValue = document.getElementById('userinput')
 
-const calculate = document
-  .querySelectorAll('.operation')
-  .forEach(function (item) {
-    item.addEventListener('click', function (e) {
-      let lastValue = inputValue.innerText.substring(
-        inputValue.innerText.length,
-        inputValue.innerText.length - 1
-      )
-      console.log(lastValue)
-      if (e.target.innerText == '=') {
-        inputValue.innerText = eval(inputValue.innerText)
-      } else if (e.target.innerText == 'AC') {
-        inputValue.innerText = '0'
-      } else if (e.target.innerText == 'DEL') {
-        inputValue.innerText = inputValue.innerText.substring(
-          0,
-          inputValue.innerText.length - 1
-        )
-        if (inputValue.innerText.length == 0) {
-          inputValue.innerText = 0
-        }
-      } else if (!isNaN(lastValue)) {
-        inputValue.innerText += e.target.innerText
-      }
-    })
-  })
-
-const number = document.querySelectorAll('.number').forEach(function (item) {
+document.querySelectorAll('.operation').forEach(function (item) {
   item.addEventListener('click', function (e) {
-    if (inputValue.innerText == '0') {
-      inputValue.innerText = ''
+    let lastValue = inputValue.value.substring(
+      inputValue.value.length - 1,
+      inputValue.value.length
+    )
+
+    if (e.target.innerText == '=') {
+      inputValue.value = eval(inputValue.value)
+    } else if (e.target.innerText == 'AC') {
+      inputValue.value = '0'
+    } else if (e.target.innerText == 'DEL') {
+      inputValue.value = inputValue.value.substring(
+        0,
+        inputValue.value.length - 1
+      )
+      if (inputValue.value.length == 0) {
+        inputValue.value = '0'
+      }
+    } else if (!isNaN(lastValue)) {
+      inputValue.value += e.target.innerText
     }
-    inputValue.innerText += e.target.innerText.trim()
+  })
+})
+
+document.querySelectorAll('.number').forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    if (inputValue.value == '0') {
+      inputValue.value = ''
+    }
+    inputValue.value += e.target.innerText.trim()
   })
 })
