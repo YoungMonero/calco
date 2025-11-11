f543e15489285a9f09fa844dd43bbd618e90e7d6
youngmonero <yungmonero29@gmail.com>
2025-11-11 16:34:49 +0100

    liter error CSS/JS

--- script/script.js
+++ script/script.js
@@ -2,21 +2,21 @@ const inputValue = document.getElementById('userinput')
 
 document.querySelectorAll('.operation').forEach(function (item) {
   item.addEventListener('click', function (e) {
-    let lastValue = inputValue.value.substring(
+    const lastValue = inputValue.value.substring(
       inputValue.value.length - 1,
       inputValue.value.length
     )
 
-    if (e.target.innerText == '=') {
+    if (e.target.innerText === '=') {
       inputValue.value = eval(inputValue.value)
-    } else if (e.target.innerText == 'AC') {
+    } else if (e.target.innerText === 'AC') {
       inputValue.value = '0'
-    } else if (e.target.innerText == 'DEL') {
+    } else if (e.target.innerText === 'DEL') {
       inputValue.value = inputValue.value.substring(
         0,
         inputValue.value.length - 1
       )
-      if (inputValue.value.length == 0) {
+      if (inputValue.value.length === 0) {
         inputValue.value = '0'
       }
     } else if (!isNaN(lastValue)) {
@@ -27,7 +27,7 @@ document.querySelectorAll('.operation').forEach(function (item) {
 
 document.querySelectorAll('.number').forEach(function (item) {
   item.addEventListener('click', function (e) {
-    if (inputValue.value == '0') {
+    if (inputValue.value === '0') {
       inputValue.value = ''
     }
     inputValue.value += e.target.innerText.trim()
